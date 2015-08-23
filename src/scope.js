1f8e2ea3d545b630cb78cb48663b0005ad28268f
RuslanGrigoryev <rus.grigoryev@yandex.ua>
2015-08-23 14:27:43 +0300

    Based value comparsion

--- src/scope.js
+++ src/scope.js
@@ -6,10 +6,11 @@ function Scope() {
 	this.$$lastDirtyWatch = null;
 }
 
-Scope.prototype.$watch = function(watchFn, listenerFn) {
+Scope.prototype.$watch = function(watchFn, listenerFn, valueEq) {
 	var watcher = {
 		watchFn: watchFn,
 		listenerFn: listenerFn || function() {},
+		valueEq: !!valueEq,
 		last: initWatchVal
 	};
 
@@ -23,9 +24,9 @@ Scope.prototype.$$digestOnce = function() {
 	_.forEach(this.$$watchers, function(watcher) {
 		newValue = watcher.watchFn(self);
 		oldValue = watcher.last;
-		if (newValue !== oldValue) {
+		if (!self.$$areEqual(newValue, oldValue, watcher.valueEq)) {
 			self.$$lastDirtyWatch = watcher;
-			watcher.last = newValue;
+			watcher.last = (watcher.valueEq ? _.cloneDeep(newValue): newValue);
 			watcher.listenerFn(newValue,
 			(oldValue === initWatchVal ? newValue : oldValue),
 			self);
@@ -50,6 +51,14 @@ Scope.prototype.$digest = function() {
 			throw "10 iterations reached";
 		}
 	} while (dirty);
+};
+
+Scope.prototype.$$areEqual = function(newValue, oldValue, valueEq) {
+	if (valueEq) {
+		return _.isEqual(newValue, oldValue);
+	} else {
+		return newValue === oldValue;
+	}
 };
 
 function initWatchVal() {}
